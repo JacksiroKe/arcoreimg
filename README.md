a3e72a7b341aedf050a3e2e24c62ad8510a92449
Jacksiro <jacksiroke@gmail.com>
2020-04-07 23:25:13 +0300

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
-# arcoreimg
-arcoreimg desktop tool
+# arcoreimg Tool
+A desktop app that implements arcoreimg 
+![arcoreimg Screenshot](https://github.com/JacksiroKe/arcoreimg/raw/master/screenshots/ARCoreImg.png "arcoreimg Screenshot")
+[arcoreimg](https://developers.google.com/ar/develop/c/augmented-images/arcoreimg)  is a command line tool that takes a set of reference images and generates an image database file. This tool is available in the [ARCore SDK for Android](https://github.com/google-ar/arcore-android-sdk/releases) and is for only Android and Android NDK developers. It is built into the Unity SDK and ARCore Unreal plugin. However if you dont like doing command line stuff when building an android app then this will do you good for Windows.
+
+## Features
+1. Check image quality
+2. Create image database from a directory of images
+3. Create a image database from an image list file
---
+++ arcoreimg-app/MainWindow.xaml.cs
@@ -0,0 +1,203 @@
+﻿using Microsoft.WindowsAPICodePack.Dialogs;
+using System;
+using System.Diagnostics;
+using System.IO;
+using System.Windows;
+
+namespace arcoreimg_app
+{
+    /// <summary>
+    /// Interaction logic for MaarciViewer.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        string ImgFilename, DirFilename, DirDatabase, ARCoreImg, NewDatabase;
+        private long _filesize;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            ARCoreImg = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "arcoreimg.exe");
+        }
+
+        private void BtnImgBrowser_Click(object sender, RoutedEventArgs e)
+        {
+            var dlg = new Microsoft.Win32.OpenFileDialog
+            {
+                Title = "Select an Image",
+                Filter = "Images |*.jpg; *.png",
+                CheckFileExists = true
+            };
+            if (dlg.ShowDialog() == true)
+            {
+                ImageUri.Text = ImgFilename = dlg.FileName;
+                CheckImage();
+            }
+        }
+
+        private void CheckImage()
+        {
+            _filesize = new FileInfo(ImgFilename).Length;
+            int fsize = int.Parse(_filesize.ToString()) / 1000000;
+            TxtFilename.Text = Path.GetFileName(ImgFilename);
+
+            Process process = new Process();
+            ProcessStartInfo startInfo = new ProcessStartInfo();
+            startInfo.FileName = "Cmd.exe";
+            startInfo.Arguments = "/C \"arcoreimg.exe eval-img --input_image_path=" + ImgFilename;
+            process.StartInfo = startInfo;
+            process.StartInfo.UseShellExecute = false;
+            process.StartInfo.RedirectStandardInput = true;
+            process.StartInfo.RedirectStandardOutput = true;
+            process.StartInfo.WindowStyle = ProcessWindowStyle.Hidden;
+            process.Start();
+
+            try
+            {
+                string result = process.StandardOutput.ReadToEnd();
+                process.WaitForExit();
+                int score = int.Parse(result);
+                LoadingBar.Value = score;
+                string ResultStr = result + "%";
+                TxtProgress.Text = ResultStr.Trim();
+
+                string f_size = "";
+                if (fsize < 100000) f_size = fsize + " MB";
+                else if (fsize > 100000) f_size = fsize + " GB";
+                else f_size = fsize + " KB";
+
+                switch (score)
+                {
+                    case 50:
+                        TxtFeedback.Text = "Image score average!";
+                        break;
+
+                    case 75:
+                        TxtFeedback.Text = "Image score above average!";
+                        break;
+
+                    case 100:
+                        TxtFeedback.Text = "Image passed test!";
+                        break;
+
+                    default:
+                        TxtFeedback.Text = "Image score poor!";
+                        break;
+                }
+            }
+            catch (Exception ex)
+            {
+                //arcoreimg.WriteLogs("App Errors", @" " + ex.Message, @"" + ex.InnerException, @"" + ex.StackTrace);
+            }
+        }
+
+        private void BtnDirBrowser_Click(object sender, RoutedEventArgs e)
+        {
+            CommonOpenFileDialog dlgLst = new CommonOpenFileDialog()
+            {
+                Title = "Select an Image Directory",
+                InitialDirectory = Path.Combine(Environment.GetEnvironmentVariable("USERPROFILE"), "Downloads"),
+                IsFolderPicker = true
+            };            
+
+            if (dlgLst.ShowDialog() == CommonFileDialogResult.Ok)
+            {
+                DirFilename = dlgLst.FileName;
+            
+                CommonOpenFileDialog dlgDb = new CommonOpenFileDialog()
+                {
+                    Title = "Select Where to save the Image Database",
+                    InitialDirectory = Path.Combine(Environment.GetEnvironmentVariable("USERPROFILE"), "Documents"),
+                    IsFolderPicker = true
+                };
+
+                if (dlgDb.ShowDialog() == CommonFileDialogResult.Ok)
+                {
+                    DirDatabase = dlgDb.FileName;
+                    NewDatabase = "myimages_" + DateTime.Now.ToString("yyyyMMddHHmmss") + ".imgdb";
+                    Process process = new Process();
+                    ProcessStartInfo startInfo = new ProcessStartInfo();
+                    startInfo.FileName = "Cmd.exe";
+                    startInfo.Arguments = "/C \"arcoreimg.exe build-db --input_images_directory=" + DirFilename + 
+                        " --output_db_path=" + DirDatabase + "/" + NewDatabase;
+
+                    process.StartInfo = startInfo;
+                    process.StartInfo.UseShellExecute = false;
+                    process.StartInfo.RedirectStandardInput = true;
+                    process.StartInfo.RedirectStandardOutput = true;
+                    process.StartInfo.WindowStyle = ProcessWindowStyle.Hidden;
+                    process.Start();
+
+                    try
+                    {
+                        string result = process.StandardOutput.ReadToEnd();
+                        process.WaitForExit();//NewDatabase, LstFilename
+                        TxtFeedback1.Text = "A new Database: " + NewDatabase + " from an Image directory: " + DirFilename +
+                            "\n\nYou can specify another directory that contains your images to create another database.";
+                    }
+                    catch (Exception ex)
+                    {
+                        //arcoreimg.WriteLogs("App Errors", @" " + ex.Message, @"" + ex.InnerException, @"" + ex.StackTrace);
+                    }
+                }
+            
+            }
+
+        }
+
+        private void BtnTxtBrowser_Click(object sender, RoutedEventArgs e)
+        {
+            var dlgLst = new Microsoft.Win32.OpenFileDialog
+            {
+                Title = "Select a File List",
+                Filter = "File List |*.txt",
+                CheckFileExists = true
+            };
+
+            if (dlgLst.ShowDialog() == true)
+            {
+                DirFilename = dlgLst.FileName;
+
+                CommonOpenFileDialog dlgDb = new CommonOpenFileDialog()
+                {
+                    Title = "Select Where to save the Image Database",
+                    InitialDirectory = Path.Combine(Environment.GetEnvironmentVariable("USERPROFILE"), "Documents"),
+                    IsFolderPicker = true
+                };
+
+                if (dlgDb.ShowDialog() == CommonFileDialogResult.Ok)
+                {
+                    DirDatabase = dlgDb.FileName;
+                    NewDatabase = "myimages_" + DateTime.Now.ToString("yyyyMMddHHmmss") + ".imgdb";
+
+                    Process process = new Process();
+                    ProcessStartInfo startInfo = new ProcessStartInfo();
+                    startInfo.FileName = "Cmd.exe";
+                    startInfo.Arguments = "/C \"arcoreimg.exe build-db --input_image_list_path=" + DirFilename +
+                        " --output_db_path=" + DirDatabase + "/" + NewDatabase;
+
+                    process.StartInfo = startInfo;
+                    process.StartInfo.UseShellExecute = false;
+                    process.StartInfo.RedirectStandardInput = true;
+                    process.StartInfo.RedirectStandardOutput = true;
+                    process.StartInfo.WindowStyle = ProcessWindowStyle.Hidden;
+                    process.Start();
+
+                    try
+                    {
+                        string result = process.StandardOutput.ReadToEnd();
+                        process.WaitForExit();//NewDatabase, LstFilename
+                        TxtFeedback2.Text = "A new database: " + NewDatabase + " from an image list file: " + DirFilename +
+                            "\n\nBrowse to another Image File List to Create a Database From";
+                    }
+                    catch (Exception ex)
+                    {
+                        //arcoreimg.WriteLogs("App Errors", @" " + ex.Message, @"" + ex.InnerException, @"" + ex.StackTrace);
+                    }
+                }
+
+            }
+        }
+
+    }
+}
